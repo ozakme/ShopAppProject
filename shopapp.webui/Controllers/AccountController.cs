8dd4bd4caa45c1fb9c11e606afc09205c646ffb0
Mehmet Akif Ozbek <ozbekmehmetakif@gmail.com>
2025-01-02 00:59:15 +0300

    Mail işlemleri, şifremi unuttum kısmı

--- shopapp.webui/Controllers/AccountController.cs
+++ shopapp.webui/Controllers/AccountController.cs
@@ -1,19 +1,25 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using shopapp.webui.EmailServices;
 using shopapp.webui.Identity;
 using shopapp.webui.Models;
 
 namespace shopapp.webui.Controllers
 {
+    [AutoValidateAntiforgeryToken]
     public class AccountController : Controller
     {
         private UserManager<User> _userManager;
         private SignInManager<User> _signInManager;
-        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
+        private IEmailSender _emailSender;
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager, IEmailSender emailSender)
         {
             _userManager = userManager;
             _signInManager = signInManager;
+            _emailSender = emailSender;
         }
         public IActionResult Login(string ReturnUrl = null)
         {
@@ -24,6 +30,7 @@ namespace shopapp.webui.Controllers
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public async Task<IActionResult> Login(LoginModel model)
         {
             if (!ModelState.IsValid)
@@ -37,6 +44,12 @@ namespace shopapp.webui.Controllers
             if (user == null)
             {
                 ModelState.AddModelError("", "Bu kullanıcı adı ile daha önce hesap oluşturulmamış");
+                return View(model);
+            }
+
+            if (!await _userManager.IsEmailConfirmedAsync(user))
+            {
+                ModelState.AddModelError("", "Lütfen email hesabınıza gelen link ile üyeliğinizi onaylayınız.");
                 return View(model);
             }
 
@@ -57,6 +70,7 @@ namespace shopapp.webui.Controllers
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public async Task<IActionResult> Register(RegisterModel model)
         {
             if (!ModelState.IsValid)
@@ -76,17 +90,133 @@ namespace shopapp.webui.Controllers
             if (result.Succeeded)
             {
                 // generate token
+                var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                var url = Url.Action("ConfirmEmail", "Account", new
+                {
+                    userId = user.Id,
+                    token = code
+                });
+
                 // email
+                await _emailSender.SendEmailAsync(model.Email, "Hesabınızı onaylayınız.", $"Lütfen email hesabınızı onaylamak için linke <a href='https://localhost:44318{url}'>tıklayınız.</a>");
                 return RedirectToAction("Login", "Account");
             }
 
             ModelState.AddModelError("", "Bilinmeyen hata oldu lütfen tekrar deneyiniz.");
             return View(model);
         }
+
+
         public async Task<IActionResult> Logout()
         {
             await _signInManager.SignOutAsync();
             return Redirect("~/");
         }
+
+        public async Task<IActionResult> ConfirmEmail(string userId, string token)
+        {
+            if (userId == null || token == null)
+            {
+                CreateMessage("Geçersiz token.", "danger");
+                return View();
+            }
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user != null)
+            {
+                var result = await _userManager.ConfirmEmailAsync(user, token);
+                if (result.Succeeded)
+                {
+                    CreateMessage("Hesabınız onaylandı.", "success");
+                    return View();
+                }
+            }
+            CreateMessage("Hesabınızı onaylanmadı.", "warning");
+            return View();
+        }
+
+        public IActionResult ForgotPassword()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ForgotPassword(string email)
+        {
+            if (string.IsNullOrEmpty(email))
+            {
+                CreateMessage("Geçersiz email adresi.", "danger");
+                return View();
+            }
+            var user = await _userManager.FindByEmailAsync(email);
+            if (user == null)
+            {
+                CreateMessage("Bu email adresi ile daha önce hesap oluşturulmamış.", "danger");
+                return View();
+            }
+            var code = await _userManager.GeneratePasswordResetTokenAsync(user);
+            var url = Url.Action("ResetPassword", "Account", new
+            {
+                userId = user.Id,
+                token = code
+            });
+            await _emailSender.SendEmailAsync(email, "Şifre sıfırlama", $"Şifrenizi sıfırlamak için linke <a href='https://localhost:44318{url}'>tıklayınız.</a>");
+            CreateMessage("Şifre sıfırlama linki email adresinize gönderildi.", "success");
+            return View();
+        }
+
+
+        public IActionResult ResetPassword(string userId, string token)
+        {
+            if (userId == null || token == null)
+            {
+                CreateMessage("Geçersiz token.", "danger");
+                return RedirectToAction("Home","Index");
+            }
+            var model = new ResetPasswordModel()
+            {
+                Token = token
+            };
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ResetPassword(ResetPasswordModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            var user = await _userManager.FindByEmailAsync(model.Email);
+            if (user == null)
+            {
+                return RedirectToAction("Home", "Index");
+            }
+
+            var result = await _userManager.ResetPasswordAsync(user, model.Token, model.Password);
+
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+
+            return View(model);
+        }
+
+
+
+
+        private void CreateMessage(string message, string alerttype)
+        {
+            var msg = new AlertMessage()
+            {
+                Message = message,
+                AlertType = alerttype
+            };
+            TempData["message"] = JsonConvert.SerializeObject(msg);
+        }
+
     }
 }
